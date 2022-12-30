68adc53a42e5d24c446152dcd5c292c91dc56176
Brian Wang <bwbrianwang1018@gmail.com>
2022-12-29 16:06:39 -0800

    Add styling to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,12 +8,15 @@ function Contact() {
     /*
     <section class="contact">
       <h1 class="header">Contact Us</h1>
-      <ContactItem />
+      <div class="contact-grid">
+        <ContactItem />
+      </div>
     </section>
     */
     // create HTML element
     const contact = document.createElement("section")
     const header = document.createElement("h1")
+    const grid = document.createElement("div")
 
     // add text
     header.textContent = "Contact Us"
@@ -21,13 +24,17 @@ function Contact() {
     // add class
     contact.className = "contact"
     header.className = "header"
+    grid.className = "contact-grid"
 
     // append/prepend
-    contact.append(
-        header,
+    grid.append(
         ContactItem(chefMaoImg, "Chef Mao", "(123)-456-7890", "chefmao@genshinimpact.com"),
         ContactItem(xianglingImg, "Xiangling", "(890)-567-1234", "xiangling@genshinimpact.com"),
         ContactItem(guobaImg, "Guoba", "(747)-698-0450", "guoba@genshinimpact.com")
+    )
+    contact.append(
+        header,
+        grid
     )
 
     return contact
--- src/initialLoad.js
+++ src/initialLoad.js
@@ -2,6 +2,7 @@ import Tabs from "./components/Tabs"
 import Home from "./components/Home"
 import Footer from "./components/Footer"
 import Menu from "./components/Menu"
+import Contact from "./components/Contact"
 
 function initialLoad() {
     const main = document.querySelector("main")
@@ -9,7 +10,8 @@ function initialLoad() {
     document.body.prepend(Tabs())
     main.append(
         // Home()
-        Menu()
+        // Menu()
+        Contact()
     )
     document.body.append(Footer())
 }
